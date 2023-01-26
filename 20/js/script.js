6e450c3c5ac2af21214a49310bca18af41f14cce
KubrikKY <kubrikkirill@gmail.com>
2023-01-26 15:56:35 +0300

    Continue

--- 20/js/script.js
+++ 20/js/script.js
@@ -1,5 +1,19 @@
 'use strict';
 
-function treeByLevels (rootNode) {
-	if ()
+function perimeter (n) {
+    let a = 1;
+    let b = 1;
+    let arr = [a, b];
+    for (let i = 3; i <= n + 1; i++) {
+      let c = a + b;
+      a = b;
+      b = c;
+      arr.push(b);
+    }
+    if (arr.length == 2) {
+        arr.pop();
+    }
+    return arr.reduce((a, b) => a + b, 0) * 4;
 }
+
+console.log(perimeter (0));
